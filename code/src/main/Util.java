713a472483ba6670da57e4d8a423a4d9548244b6
Zsombor Fedor <zsombor.fedor@icloud.com>
2018-03-19 02:16:10 +0100

    Util osztály az esetekkel

---
+++ code/src/main/Util.java
@@ -0,0 +1,130 @@
+package main;
+
+import java.util.Scanner;
+
+public class Util {
+    public static void main(String[] args) {
+
+        System.out.println("Melyik szekvenciát szeretnéd elindítani? (1-16)");
+
+
+        Scanner reader = new Scanner(System.in);
+        int n = reader.nextInt();
+
+        if (n == 1)
+            _1();
+
+        if (n == 2)
+            _2();
+
+        if (n == 3)
+            _3();
+
+        if (n == 4)
+            _4();
+
+        if (n == 5)
+            _5();
+
+        if (n == 6)
+            _6();
+
+        if (n == 7)
+            _7();
+
+        if (n == 8)
+            _8();
+
+        if (n == 9)
+            _9();
+
+        if (n == 10)
+            _10();
+
+        if (n == 11)
+            _11();
+
+        if (n == 12)
+            _12();
+
+        if (n == 13)
+            _13();
+
+        if (n == 14)
+            _14();
+
+        if (n == 15)
+            _15();
+
+        if (n == 16)
+            _16();
+
+
+    }
+    static void _1(){
+
+    }
+
+    static void _2(){
+
+    }
+
+    static void _3(){
+
+    }
+
+    static void _4(){
+
+    }
+
+    static void _5(){
+
+    }
+
+    static void _6(){
+
+    }
+
+    static void _7(){
+
+    }
+
+    static void _8(){
+
+    }
+
+    static void _9(){
+
+    }
+
+    static void _10(){
+
+    }
+
+    static void _11(){
+
+    }
+
+    static void _12(){
+
+    }
+
+    static void _13(){
+
+    }
+
+    static void _14(){
+
+    }
+
+    static void _15(){
+
+    }
+
+    static void _16(){
+
+    }
+
+}
+
+
